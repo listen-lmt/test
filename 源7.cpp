94b159467ca87da90649c8ea00ef83e80a211649
listen-lmt <124659616+listen-lmt@users.noreply.github.com>
2023-02-08 16:31:55 +0800

    Add files via upload

---
+++ 源7.cpp
@@ -0,0 +1,12 @@
+﻿“Project2.exe”(Win32): 已加载“D:\code\Project2\x64\Debug\Project2.exe”。已加载符号。
+“Project2.exe”(Win32): 已加载“C:\Windows\System32\ntdll.dll”。
+“Project2.exe”(Win32): 已加载“C:\Windows\System32\kernel32.dll”。
+“Project2.exe”(Win32): 已加载“C:\Windows\System32\KernelBase.dll”。
+“Project2.exe”(Win32): 已加载“C:\Windows\System32\vcruntime140d.dll”。
+“Project2.exe”(Win32): 已加载“C:\Windows\System32\ucrtbased.dll”。
+线程 0x3de4 已退出，返回值为 0 (0x0)。
+“Project2.exe”(Win32): 已加载“C:\Windows\System32\kernel.appcore.dll”。
+“Project2.exe”(Win32): 已加载“C:\Windows\System32\msvcrt.dll”。
+线程 0x5050 已退出，返回值为 0 (0x0)。
+线程 0x1140 已退出，返回值为 0 (0x0)。
+程序“[8868] Project2.exe”已退出，返回值为 0 (0x0)。
